4effa559c080ffe8c7a8a9f8244c4979e4c3cbba
Aleksandar Terentic <aleksandar.terentic@seavus.com>
2015-10-29 11:31:21 +0100

    Added initial emacs config.

---
+++ emacs/init.el
@@ -0,0 +1,47 @@
+(require 'package)
+
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/"))
+
+(package-initialize)
+
+;;; install required packages
+(defvar packages '(paredit
+		   auto-complete
+		   yasnippet
+		   js2-mode
+		   js2-refactor
+		   ac-js2
+		   markdown-mode
+		   clojure-mode
+		   cider
+		   haskell-mode))
+
+(dolist (package packages)
+  (unless (package-installed-p package)
+    (package-install package)))
+
+(add-to-list 'auto-mode-alist '("\\.json$" . js-mode))
+
+(add-hook 'js-mode-hook 'js2-minor-mode)
+(add-hook 'js2-mode-hook 'ac-js2-mode)
+
+(setq js2-highlight-level 3)
+
+;;; yasnippet
+;;; should be loaded before auto complete so that they can work together
+(require 'yasnippet)
+(yas-global-mode 1)
+
+;;; auto complete mod
+;;; should be loaded after yasnippet so that they can work together
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
+(ac-config-default)
+;;; set the trigger key so that it can work together with yasnippet on tab key,
+;;; if the word exists in yasnippet, pressing tab will cause yasnippet to
+;;; activate, otherwise, auto-complete will
+(ac-set-trigger-key "TAB")
+(ac-set-trigger-key "<tab>")
+
+(tool-bar-mode -1)
