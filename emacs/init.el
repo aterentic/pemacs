fdc24f2b209644fbc7f388758ac86c4d85cd3bab
Aleksandar Terentic <aterentic@gmail.com>
2016-01-10 10:02:43 +0100

    Changed default tab-width for js-mode.

--- emacs/init.el
+++ emacs/init.el
@@ -35,6 +35,7 @@
 
 (add-to-list 'auto-mode-alist '("\\.json$" . js-mode))
 
+(add-hook 'js-mode-hook (lambda () (setq tab-width 4)))
 (add-hook 'js-mode-hook 'js2-minor-mode)
 (add-hook 'js2-mode-hook 'ac-js2-mode)
 
