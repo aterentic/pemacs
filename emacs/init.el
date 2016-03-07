5fba719ab278c85f766c4873bf2daa95e1a81352
Aleksandar Terentic <aterentic@gmail.com>
2016-03-07 21:16:40 +0100

    Changed package repositories.

--- emacs/init.el
+++ emacs/init.el
@@ -1,11 +1,10 @@
 (require 'package)
 
-(add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/") t)
-(add-to-list 'package-archives
-             '("tromey" . "http://tromey.com/elpa/") t)
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(setq package-archives
+      '(("gnu" . "http://elpa.gnu.org/packages/")
+	("marmalade" . "http://marmalade-repo.org/packages/")
+	("melpa" . "http://melpa.milkbox.net/packages/")
+	("tromey" . "http://tromey.com/elpa/")))
 
 (package-initialize)
 
