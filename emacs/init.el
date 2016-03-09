80b85bd1542f34813e3f686675eb7008c47e95b3
Aleksandar Terentic <aleksandar.terentic@seavus.com>
2016-03-09 12:34:59 +0100

    Changed IDO settings.

--- emacs/init.el
+++ emacs/init.el
@@ -70,20 +70,11 @@
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
 
+(setq ido-enable-flex-matching t)
+(setq ido-everywhere t)
+(ido-mode 1)
+
 (setq user-full-name "Aleksandar Terentic")
 (setq user-mail-address "aterentic@gmail.com")
 
 (toggle-frame-fullscreen)
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ido-mode (quote both) nil (ido)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
