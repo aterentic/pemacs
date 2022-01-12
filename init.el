52b8c53bf38255f5e12ca40d1eb630b9cef979b2
Aleksandar Terentić <aterentic@gmail.com>
2022-01-12 23:38:51 +0100

    Set grep-find-ignored-directories.

--- init.el
+++ init.el
@@ -25,7 +25,6 @@
 
 (setq backup-directory-alist `((".*" . ,temporary-file-directory)))
 (setq auto-save-file-name-transforms `((".*" ,temporary-file-directory t)))
-(setq grep-find-ignored-directories '(".git" "vendor" "node_modules"))
 
 (global-set-key (kbd "M-o") 'other-window)
 (global-set-key (kbd "C-x C-;") 'comment-or-uncomment-region)
@@ -40,7 +39,9 @@
 
 (delete-selection-mode t)
 (desktop-save-mode 1)
-(server-mode)
+;;; (server-mode)
+
+(setq require-final-newline t)
 
 ;;; package archives
 (require 'package)
@@ -48,6 +49,7 @@
 ;; stable picks up only tags from github.com
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/") t)
 
 (package-initialize)
 
@@ -67,7 +69,8 @@
 ;;; themes: solarized-theme, material-theme, monokai-theme
 (use-package monokai-theme
   :config
-  (load-theme 'monokai t))
+  (load-theme 'monokai t)
+  (set-face-background 'default "#1f201b"))
 
 ;;; modeline
 (use-package powerline
@@ -99,6 +102,11 @@
   :config
   (global-hl-line-mode t))
 
+(use-package exec-path-from-shell)
+  ;; :config
+  ;; (when (memq window-system '(mac ns x))
+  ;;   (exec-path-from-shell-initialize)))
+
 ;;; org-mode
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
@@ -121,7 +129,6 @@
 
 (use-package deft
   :config
-  (define-key global-map "\C-c\C-d" 'deft)
   (setq deft-extensions '("org"))
   (setq deft-use-filename-as-title t)
   (setq deft-use-filter-string-for-filename t)
@@ -150,14 +157,7 @@
 
 (use-package dedicated)
 
-(use-package pocket-reader)
-
-;;; (use-package sonic-pi)
-
 (use-package pdf-tools)
-
-(use-package yasnippet
-  :commands yas-minor-mode)
 
 (use-package helm
   :bind
@@ -167,37 +167,90 @@
   :config
   (helm-mode 1))
 
+(use-package marginalia
+  :config
+  (marginalia-mode))
+
+(use-package embark
+  :bind
+  (("C-;" . embark-act)         ;; pick some comfortable binding
+   ("C-:" . embark-dwim)        ;; good alternative: M-.
+   ("C-h B" . embark-bindings)) ;; alternative for `describe-bindings'
+  :init
+  ;; Optionally replace the key help with a completing-read interface
+  (setq prefix-help-command #'embark-prefix-help-command)
+  :config
+  ;; Hide the mode line of the Embark live/completions buffers
+  (add-to-list 'display-buffer-alist
+               '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
+                 nil
+                 (window-parameters (mode-line-format . none)))))
+
 (use-package magit
   :bind
   (("C-x g" . magit-status)))
+
+(use-package yasnippet
+  :config
+  (yas-reload-all)
+  (add-hook 'prog-mode-hook 'yas-minor-mode)
+  (add-hook 'text-mode-hook 'yas-minor-mode))
+
+(use-package company
+  :custom
+  (company-idle-delay 0.25) ;; how long to wait until popup
+  ;; (company-begin-commands nil) ;; uncomment to disable popup
+  :bind
+  (:map company-active-map
+	      ("C-n". company-select-next)
+	      ("C-p". company-select-previous)
+	      ("M-<". company-select-first)
+	      ("M->". company-select-last)))
 
 (use-package flycheck
   :config
   (global-flycheck-mode))
 
-(use-package company
-  :config
-  (setq company-idle-delay 0)
-  (setq company-minimum-prefix-length 1))
-
 (use-package projectile)
+
+(defun rk/rustic-mode-hook ()
+  ;; so that run C-c C-c C-r works without having to confirm, but don't try to
+  ;; save rust buffers that are not file visiting. Once
+  ;; https://github.com/brotzeit/rustic/issues/253 has been resolved this should
+  ;; no longer be necessary.
+  (when buffer-file-name
+    (setq-local buffer-save-without-query t)))
 
 (use-package lsp-mode
   :config
-  (setq lsp-file-watch-threshold 20000)
-  :commands lsp lsp-deferred
+  ;; (setq lsp-file-watch-threshold 20000)
+  (add-hook 'lsp-mode-hook 'lsp-ui-mode)
+  :commands lsp
+  :custom
+  ;; what to use when checking on-save. "check" is default, I prefer clippy
+  (lsp-rust-analyzer-cargo-watch-command "clippy")
+  (lsp-eldoc-render-all t)
+  (lsp-idle-delay 0.6)
+  ;; This controls the overlays that display type and other hints inline. Enable
+  ;; / disable as you prefer. Well require a `lsp-workspace-restart' to have an
+  ;; effect on open projects.
+  (lsp-rust-analyzer-server-display-inlay-hints t)
+  (lsp-rust-analyzer-display-lifetime-elision-hints-enable "skip_trivial")
+  (lsp-rust-analyzer-display-chaining-hints t)
+  (lsp-rust-analyzer-display-lifetime-elision-hints-use-parameter-names nil)
+  (lsp-rust-analyzer-display-closure-return-type-hints t)
+  (lsp-rust-analyzer-display-parameter-hints nil)
+  (lsp-rust-analyzer-display-reborrow-hints nil)
   :hook
   (css-mode . lsp-deferred))
 
 (use-package lsp-ui
-  :hook
-  (lsp-mode . lsp-ui-mode)
-  :config
-  (setq lsp-ui-doc-enable nil
-	lsp-ui-peek-enable t
-	lsp-ui-sideline-enable t
-	lsp-ui-imenu-enable t
-	lsp-ui-flycheck-enable t))
+  :ensure
+  :commands lsp-ui-mode
+  :custom
+  (lsp-ui-peek-always-show t)
+  (lsp-ui-sideline-show-hover nil)
+  (lsp-ui-doc-enable nil))
 
 (global-display-line-numbers-mode)
 (setq display-line-numbers-type 'relative)
@@ -218,7 +271,7 @@
 (use-package elm-mode
   :hook
   (elm-mode . lsp-deferred)
-  (elm-mode . (lambda () (setq indent-tabs-mode nil))))
+  (elm-mode . (lambda () (setq-local indent-tabs-mode nil))))
 
 (use-package flycheck-elm
  :hook
@@ -232,25 +285,42 @@
 (use-package idris-mode)
 
 ;;; rust
-(use-package rust-mode
-  :bind
-  (("C-c C-c" . rust-compile)
-   ("C-c C-t" . rust-test))
+;; (use-package rustic)
+;;   ;; :config
+;;   ;; (setq rustic-format-on-save t))
+
+(use-package rustic
+  :ensure
+  :bind (:map rustic-mode-map
+              ("M-j" . lsp-ui-imenu)
+              ("M-?" . lsp-find-references)
+              ("C-c C-c l" . flycheck-list-errors)
+              ("C-c C-c a" . lsp-execute-code-action)
+              ("C-c C-c r" . lsp-rename)
+              ("C-c C-c q" . lsp-workspace-restart)
+              ("C-c C-c Q" . lsp-workspace-shutdown)
+              ("C-c C-c s" . lsp-rust-analyzer-status)
+              ("C-c C-c e" . lsp-rust-analyzer-expand-macro)
+              ("C-c C-c d" . dap-hydra)
+              ("C-c C-c h" . lsp-ui-doc-glance))
   :config
-  (setq indent-tabs-mode nil)
-  (setq rust-format-on-save t)
-  :hook
-  (rust-mode . lsp-deferred))
+  ;; uncomment for less flashiness
+  ;; (setq lsp-eldoc-hook nil)
+  ;; (setq lsp-enable-symbol-highlighting nil)
+  ;; (setq lsp-signature-auto-activate nil)
+
+  ;; comment to disable rustfmt on save
+  (setq rustic-format-on-save t)
+  (add-hook 'rustic-mode-hook 'rk/rustic-mode-hook))
 
 ;; golang
 (use-package go-mode
   :bind
-  ("M-," . compile)
-  ("M-." . godef-jump)
+  ;; TODO, local: ("M-," . compile)
+  ;; TODO, local: ("M-." . godef-jump)
   :hook
-  (go-mode . yas-minor-mode)
   (go-mode . lsp-deferred)
-  (before-save . (lambda () (lsp-format-buffer) (lsp-organize-imports)))
+  ;; TODO, make it go-mode local: (before-save . (lambda () (lsp-format-buffer) (lsp-organize-imports)))
   :config
   (setq lsp-gopls-staticcheck t)
   (setq lsp-gopls-complete-unimported t)
@@ -265,13 +335,13 @@
   (add-to-list 'auto-mode-alist '("\\.jsx?\\'" . web-mode))
   (flycheck-add-mode 'javascript-eslint 'web-mode)
   :hook
-  (web-mode . (lambda () (setq tab-width 4) (setq web-mode-code-indent-offset 2))))
+  (web-mode . (lambda () (setq-local tab-width 4) (setq web-mode-code-indent-offset 2))))
 
 (use-package js2-mode
   :config
   (setq js2-highlight-level 3)
   :hook
-  (js-mode . (lambda () (setq tab-width 4)))
+  (js-mode . (lambda () (setq-local tab-width 4)))
   (js-mode . js2-minor-mode))
 (use-package js2-refactor
   :config
@@ -308,10 +378,8 @@
 ;; (use-package clojure-mode)
 ;; (use-package cider)
 
-(use-package exec-path-from-shell)
-  ;; :config
-  ;; (when (memq window-system '(mac ns x))
-  ;;   (exec-path-from-shell-initialize)))
+
+(setq grep-find-ignored-directories (append grep-find-ignored-directories '("node_modules")))
 
 ;;; local defaults
 (if (file-exists-p "~/.emacs.d/default.el") (load-file "~/.emacs.d/default.el"))
